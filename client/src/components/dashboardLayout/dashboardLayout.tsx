b5dcfb9c36249fcd40de87a568131bc19c17bde7
oppong nicholsd <nictech23@gmail.com>
2024-05-05 21:54:48 +0000

    add

---
+++ client/src/components/dashboardLayout/dashboardLayout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DashboardLayout = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default DashboardLayout
---
+++ client/src/screen/farm-data/farm-data.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Farm_data = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Farm_data
